8603161c38d31eec0486b283ecc7ef35b75c3ccf
Mahmoud Al-Saqqa <mahmoudmohammed11112001@gmail.com>
2025-08-16 13:22:31 +0300

    إصلاح نافذة اقتطاع الصورة للهاتف

--- src/components/layout/MobileBottomNavigation.tsx
+++ src/components/layout/MobileBottomNavigation.tsx
@@ -18,7 +18,7 @@ const MobileBottomNavigation: React.FC = () => {
   ];
 
   return (
-    <nav className="fixed bottom-0 left-0 right-0 bg-white border-t border-gray-200 z-50 md:hidden">
+    <nav className="fixed bottom-0 left-0 right-0 bg-white border-t border-gray-200 z-40 md:hidden">
       <div className="flex justify-around items-center h-16 px-2">
         {navigationItems.map((item) => {
           const Icon = item.icon;
